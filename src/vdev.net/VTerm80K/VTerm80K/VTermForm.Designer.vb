e696a05abef2246470e11c61188ed76bf7c76e71
Toukuro <kj_adachi@nifty.com>
2018-11-11 08:51:20 +0900

    コンソール画面作成中 キーボードイメージ作成中

---
+++ src/vdev.net/VTerm80K/VTerm80K/VTermForm.Designer.vb
@@ -0,0 +1,270 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class VTermForm
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlDisplay = New System.Windows.Forms.Panel()
+        Me.pnlKeyBoard = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.pnlKeyBoard.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlDisplay
+        '
+        Me.pnlDisplay.Location = New System.Drawing.Point(1, 1)
+        Me.pnlDisplay.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlDisplay.Name = "pnlDisplay"
+        Me.pnlDisplay.Size = New System.Drawing.Size(640, 400)
+        Me.pnlDisplay.TabIndex = 0
+        '
+        'pnlKeyBoard
+        '
+        Me.pnlKeyBoard.Controls.Add(Me.Button16)
+        Me.pnlKeyBoard.Controls.Add(Me.Button15)
+        Me.pnlKeyBoard.Controls.Add(Me.Button14)
+        Me.pnlKeyBoard.Controls.Add(Me.Button13)
+        Me.pnlKeyBoard.Controls.Add(Me.Button12)
+        Me.pnlKeyBoard.Controls.Add(Me.Button11)
+        Me.pnlKeyBoard.Controls.Add(Me.Button10)
+        Me.pnlKeyBoard.Controls.Add(Me.Button9)
+        Me.pnlKeyBoard.Controls.Add(Me.Button8)
+        Me.pnlKeyBoard.Controls.Add(Me.Button7)
+        Me.pnlKeyBoard.Controls.Add(Me.Button6)
+        Me.pnlKeyBoard.Controls.Add(Me.Button5)
+        Me.pnlKeyBoard.Controls.Add(Me.Button4)
+        Me.pnlKeyBoard.Controls.Add(Me.Button3)
+        Me.pnlKeyBoard.Controls.Add(Me.Button2)
+        Me.pnlKeyBoard.Controls.Add(Me.Button1)
+        Me.pnlKeyBoard.Location = New System.Drawing.Point(1, 401)
+        Me.pnlKeyBoard.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlKeyBoard.Name = "pnlKeyBoard"
+        Me.pnlKeyBoard.Size = New System.Drawing.Size(640, 242)
+        Me.pnlKeyBoard.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1, 1)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(38, 38)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "１"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(41, 1)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(38, 38)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "２"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(81, 1)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(38, 38)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "３"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(121, 1)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(38, 38)
+        Me.Button4.TabIndex = 0
+        Me.Button4.Text = "４"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(161, 1)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(38, 38)
+        Me.Button5.TabIndex = 0
+        Me.Button5.Text = "５"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(201, 1)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(38, 38)
+        Me.Button6.TabIndex = 0
+        Me.Button6.Text = "６"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(241, 1)
+        Me.Button7.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(38, 38)
+        Me.Button7.TabIndex = 0
+        Me.Button7.Text = "７"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(281, 1)
+        Me.Button8.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(38, 38)
+        Me.Button8.TabIndex = 0
+        Me.Button8.Text = "８"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(321, 1)
+        Me.Button9.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(38, 38)
+        Me.Button9.TabIndex = 0
+        Me.Button9.Text = "９"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(361, 1)
+        Me.Button10.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(38, 38)
+        Me.Button10.TabIndex = 0
+        Me.Button10.Text = "０"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(401, 1)
+        Me.Button11.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(38, 38)
+        Me.Button11.TabIndex = 0
+        Me.Button11.Text = "－"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(441, 1)
+        Me.Button12.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(38, 38)
+        Me.Button12.TabIndex = 0
+        Me.Button12.Text = "□"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(481, 1)
+        Me.Button13.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(38, 38)
+        Me.Button13.TabIndex = 0
+        Me.Button13.Text = "□"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(521, 1)
+        Me.Button14.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(38, 38)
+        Me.Button14.TabIndex = 0
+        Me.Button14.Text = "□"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(561, 1)
+        Me.Button15.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(38, 38)
+        Me.Button15.TabIndex = 0
+        Me.Button15.Text = "□"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.Location = New System.Drawing.Point(601, 1)
+        Me.Button16.Margin = New System.Windows.Forms.Padding(1)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(38, 38)
+        Me.Button16.TabIndex = 0
+        Me.Button16.Text = "月"
+        Me.Button16.UseVisualStyleBackColor = True
+        '
+        'VTermForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(643, 643)
+        Me.Controls.Add(Me.pnlKeyBoard)
+        Me.Controls.Add(Me.pnlDisplay)
+        Me.Name = "VTermForm"
+        Me.Text = "Form1"
+        Me.pnlKeyBoard.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlDisplay As Panel
+    Friend WithEvents pnlKeyBoard As Panel
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+End Class
---
+++ src/vdev.net/VTerm80K/VTerm80K/VTermForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class VTermForm
+
+End Class
